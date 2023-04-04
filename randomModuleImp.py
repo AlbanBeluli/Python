c485b57c56433f3fe3f8c25aaa0ea257befdfd14
AlbanBeluli <126069240+AlbanBeluli@users.noreply.github.com>
2023-04-04 16:24:19 +0200

    Create randomModuleImp.py

---
+++ randomModuleImp.py
@@ -0,0 +1,11 @@
+import random
+
+num = random.randint(1, 10)
+
+def user_name():
+    name = input("Enter your name: ")
+    name = name[::-1]
+    username = name + str(num)
+    return username
+
+print(user_name())
