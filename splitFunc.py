80f9689b64b0998fde2e98ed5d170672e3276140
AlbanBeluli <126069240+AlbanBeluli@users.noreply.github.com>
2023-04-04 16:32:54 +0200

    Update splitFunc.py

--- splitFunc.py
+++ splitFunc.py
@@ -6,3 +6,11 @@ def count_words(arr: str):
     return f"There are {len(words)} words in the sentence."
 
 print(count_words("Hello, I am learning Python!"))
+
+
+
+def count_characters(a):
+    a = a.replace(' ', '') 
+    return f'The string has ' \
+           f'{len(a)} elements '
+print(count_characters('I love learning'))
