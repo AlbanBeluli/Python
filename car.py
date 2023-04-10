38f039668684ec9cf411602356f270f379520d59
AlbanBeluli <126069240+AlbanBeluli@users.noreply.github.com>
2023-04-10 09:42:36 +0200

    Update car.py

--- car.py
+++ car.py
@@ -2,11 +2,13 @@ class Car():
     def __init__(self, color, use_type):
         self.color = color
         self.use_type = use_type
-        self.description = color + " " + use_type
 
-my_car = Car("Red", "Sports Car")
+    def description(self):
+        return self.color + " " + self.use_type
+
+
+my_car = Car("Blue", "Sports Car")
 
 print("Color:", my_car.color)
 print("Use Type:", my_car.use_type)
-print("Description:", my_car.description)
-
+print("Description:", my_car.description())
