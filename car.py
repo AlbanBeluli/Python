6cdd4b47ec71a65a847d54d61bf5bf86702f99a7
AlbanBeluli <126069240+AlbanBeluli@users.noreply.github.com>
2023-04-10 09:52:27 +0200

    Update car.py

--- car.py
+++ car.py
@@ -6,9 +6,16 @@ class Car():
     @property
     def description(self):
         return self.color + " " + self.use_type
+    
+    @description.setter
+    def description(self, desc):
+        color, use_type = desc.split(" ")
+        self.color = color
+        self.use_type = use_type
 
 
 my_car = Car("Blue", "Sports Car")
+my_car.description = "Blue Van"
 
 print("Color:", my_car.color)
 print("Use Type:", my_car.use_type)
